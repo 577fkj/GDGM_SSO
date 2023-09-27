51fbc7598c1c1b779d954167e3f3ebf4b54741fb
方块君 <86393520+577fkj@users.noreply.github.com>
2023-09-27 20:36:34 +0800

    Update main.py
    今天单点登录服务不知为啥挂了
    增加学工号方式登录

--- main.py
+++ main.py
@@ -141,13 +141,21 @@ class card:
     }
     power_url = 'https://carduser.gdgm.cn/powerfee/index'
     sso_login_url = 'https://cardsso.gdgm.cn//login?extFlag=false&needBind=false&redirectUrl=https%3A%2F%2Fcarduser.gdgm.cn%2Fpowerfee%2Findex&plat=gdgm&'
+    card_sso_login_index_url = 'https://cardsso.gdgm.cn//login?extFlag=false&needBind=false&redirectUrl=https%3A%2F%2Fcarduser.gdgm.cn%2Fpowerfee%2Findex&plat=sso&'
+    card_sso_login_url = 'https://cardsso.gdgm.cn/doLogin'
+    card_cap_url = 'https://cardsso.gdgm.cn/captchaCode'
     power_balance_url = 'https://carduser.gdgm.cn/powerfee/getBalance'
     session = requests.Session()
 
-    def __init__(self, sso_obj):
-        self.sso_obj = sso_obj
+    def __init__(self, sso_obj, user = '', passwd = ''):
         self.__load_cookies()
-        self.__login()
+        if sso_obj is None:
+            self.user = user
+            self.passwd = passwd
+            self.__sso_login()
+        else:
+            self.sso_obj = sso_obj
+            self.__login()
         self.__save_cookies()
 
     def __save_cookies(self):
@@ -158,6 +166,41 @@ class card:
         if os.path.exists('card_cookies'):
             with open('card_cookies', 'rb') as f:
                 self.session.cookies.update(pickle.load(f))
+
+    def __sso_login(self):
+        print('使用学工号登录')
+        response = self.session.get(self.card_sso_login_index_url, headers=self.headers, allow_redirects=False)
+        if response.status_code == 302:
+            if 'token' in response.headers["Location"]:
+                print('一卡通已登录')
+            else:
+                raise Exception('一卡通登录失败')
+        else:
+            img = self.session.get(self.card_cap_url, headers=self.headers).content
+            with open('cap.png', 'wb') as f:
+                f.write(img)
+            code = ocr_code(img)
+            print(f'验证码为 {code}')
+            data = {
+                "loginType": "rftSigner",
+                "account": self.user,
+                "password": self.passwd,
+                "captchaCode": code,
+                "needBind":"",
+                "bindPlatform":"",
+                "openid":"",
+                "unionid":"",
+                "alipayUserid":"",
+                "ddUserid":"",
+                "t": "5",
+                "renter":""
+            }
+            response = self.session.post(self.card_sso_login_url, data=data, headers=self.headers).json()
+            if response['code'] == 200:
+                print('一卡通登录成功')
+            else:
+                print('一卡通登录失败 ' + response['msg'])
+
 
     def __login(self):
         response = self.session.get(self.sso_login_url, headers=self.headers, allow_redirects=False)
@@ -227,6 +270,7 @@ class data_sheet:
 
 g_user = '' # You need to input
 g_passwd = '' # You need to input
+g_card_passwd = '' # You need to input
 # 白云、荔湾校区 CHANGGONGGONGMAO 天河校区 MINGHANNORMAL
 g_impl = '' # You need to input
 # POST https://carduser.gdgm.cn/powerfee/getRoomInfo?from=&token={token}&implType={impl}
@@ -237,9 +281,15 @@ g_room = '' # You need to input
 def main():
     data = data_sheet('data.csv', ['room', 'powerBalance', "diff_h", "diff", 'time'])
 
-    sso_obj = sso(g_user, g_passwd)
+    # noinspection PyBroadException
+    try:
+        print('sso开始登录')
+        sso_obj = sso(g_user, g_passwd)
+    except Exception:
+        print('sso登录异常')
+        sso_obj = None
 
-    card_obj = card(sso_obj)
+    card_obj = card(sso_obj, g_user, g_card_passwd)
     power_data = card_obj.get_power_balance(g_impl, g_no, g_room)
 
     diff_h = 0
